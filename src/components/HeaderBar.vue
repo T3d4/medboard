024de435677bd662101603ebed70834405532753
tedabytez <tdansa64@gmail.com>
2023-08-28 11:37:40 +0100

    minor update to the header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="w-full bg-dashboard-secondary flex flex-row justify-between">
+  <div class="w-full bg-dashboard-primary border-x-[30px] border-s-dashboard-primary border-r-dashboard-primary flex flex-wrap items-center relative justify-between header-shadow">
     <div class="flex justify-between items-center h-[70px] text-white">
       <BurgerButton @burger="toggle" />
     </div>
-    <div class="my-auto mr-4">
-      <ul class="my-auto mr-4 items-center text-white flex mx-auto list-none">
+    <div >
+      <ul class="flex flex-wrap items-center text-white h-full list-none">
         <li class="ml-6">
           <router-link to="">
-            <span class="material-symbols-outlined align-middle text-[30px]"> calendar_month </span>
+            <span class="material-symbols-outlined align-sub px-1 text-[25px]"> calendar_month </span>
             Make an appointment
           </router-link>
         </li>
         <li class="ml-6">
           <router-link to="">
-            <span class="material-symbols-outlined align-middle text-[30px]"> edit </span>
-            Write a prescribtion
+            <span class="material-symbols-outlined align-sub px-1 text-[25px]"> edit </span>
+              Write a prescribtion
           </router-link>
         </li>
         <li class="ml-6">
           <router-link class="" to="">
-            <span class="material-symbols-outlined align-middle text-[30px]"> note_alt </span>
+            <span class="material-symbols-outlined align-sub px-1 text-[25px]"> note_alt </span>
             Generate Report
           </router-link>
         </li>
         <li class="ml-6 justify-center">
           <router-link to="">
-            <span class="material-symbols-outlined text-[30px]"> notifications </span>
+            <span class="material-symbols-outlined align-middle text-[30px]"> notifications </span>
           </router-link>
         </li>
-        <li class="ml-6">
+        <li class="ml-6 px-1 ">
           <router-link to="">
             <img
               src="https://slidesigma.nyc/templatemonster/angular/medboard/assets/img/dashboard/doctor-3.jpg"
-              class="rounded-full w-[40px] align-middle float-right"
+              class="rounded-full w-[40px] float-right"
             />
           </router-link>
         </li>
@@ -55,5 +55,9 @@ const toggle = () => {
 <style scoped>
 #app {
   text-align: center;
+}
+
+.header-shadow{
+  box-shadow: -5px 0px 18px #0579bdd5;
 }
 </style>
