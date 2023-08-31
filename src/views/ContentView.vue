7b2f6b8a41412423456b61270b91f9ca2eade247
tedabytez <tdansa64@gmail.com>
2023-08-31 14:54:55 +0100

    minor updates

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="text-center">Dashboard</div>
   <GenereateReport />
+  <PrescriptionModal/>
 </template>
 
 <script setup>
 import GenereateReport from '../components/GenereateReport.vue'
+import PrescriptionModal from '../components/PrescriptionModal.vue';
 </script>
 
 <style lang="scss" scoped></style>
