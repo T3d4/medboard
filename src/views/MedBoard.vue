677b442b689039c92c4a750d58bf0949b24c82a3
tedabytez <tdansa64@gmail.com>
2023-09-01 16:39:46 +0100

    medboard blueprint

---
+++ src/views/MedBoard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="px-4 py-6">
+    <div class="flex flex-row pl-5">
+      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
+      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
+      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
+      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
+    </div>
+    <div class="flex flex-row px-4 py-6">
+      <div class="basis-1/3">
+        <div class="flex flex-col">
+          <div class="basis-1/3 mb-8 max-w-[33.333%]">card 1</div>
+          <div class="basis-1/3 mb-8 max-w-[33.333%]">card 2</div>
+          <div class="basis-1/3 mb-8 max-w-[33.333%]">card 3</div>
+        </div>
+      </div>
+      <div class="basis-1/3">Hola Comrade</div>
+      <div class="basis-1/3">Hola Comrade</div>
+    </div>
+    <div class="flex flex-row px-4 py-6">
+    <div class="basis-1/2">3Rd Row</div>
+    <div class="basis-1/2">3Rd Row</div>
+    </div>
+    <div class="flex flex-row px-4 py-6">
+    <div class="basis-1/2">4th Row</div>
+    <div class="basis-1/2">4th Row</div>
+    </div>
+    <div class="flex flex-row px-4 py-6">
+    <div class="basis-3/4">5th Row</div>
+    <div class="basis-1/4">5th Row</div>
+    </div>
+    <div class="flex flex-row px-4 py-6">
+    <div class="basis-1/2">6th Row</div>
+    <div class="basis-1/2">6th Row</div>
+    </div>
+    <div class="flex flex-row px-4 py-6">
+    <div class="basis-3/4">7th Row</div>
+    <div class="basis-1/4">7th Row</div>
+    </div>
+  </div>
+</template>
