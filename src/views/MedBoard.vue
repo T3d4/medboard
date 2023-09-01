47845644b390f3305e3dc1f1d9c9d3661584440d
tedabytez <tdansa64@gmail.com>
2023-09-01 16:58:23 +0100

    minor updates

--- src/views/MedBoard.vue
+++ src/views/MedBoard.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="px-4 py-6">
     <div class="flex flex-row pl-5">
-      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
-      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
-      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
-      <div class="basis-1/4 max-w-[25%]">Hello Comrade</div>
+      <div class="basis-1/4 max-w-[25%]">
+        <router-link to="">
+          <div>
+            <div>Hello Comrade</div>
+          </div>
+        </router-link>
+      </div>
+      <div class="basis-1/4 max-w-[25%]">
+        <router-link to="">
+          <div>
+            <div>Hello Comrade</div>
+          </div>
+        </router-link>
+      </div>
+      <div class="basis-1/4 max-w-[25%]">
+        <router-link to="">
+          <div>
+            <div>Hello Comrade</div>
+          </div>
+        </router-link>
+      </div>
+      <div class="basis-1/4 max-w-[25%]">
+        <router-link to="">
+          <div>
+            <div>Hello Comrade</div>
+          </div>
+        </router-link>
+      </div>
     </div>
     <div class="flex flex-row px-4 py-6">
       <div class="basis-1/3">
@@ -14,28 +38,28 @@
           <div class="basis-1/3 mb-8 max-w-[33.333%]">card 3</div>
         </div>
       </div>
-      <div class="basis-1/3">Hola Comrade</div>
-      <div class="basis-1/3">Hola Comrade</div>
+      <div class="basis-1/3 max-w-[33.333%]">Hola Comrade</div>
+      <div class="basis-1/3 max-w-[33.333%]">Hola Comrade</div>
     </div>
     <div class="flex flex-row px-4 py-6">
-    <div class="basis-1/2">3Rd Row</div>
-    <div class="basis-1/2">3Rd Row</div>
+      <div class="basis-1/2 max-w-[50%]">3Rd Row</div>
+      <div class="basis-1/2 max-w-[50%]">3Rd Row</div>
     </div>
     <div class="flex flex-row px-4 py-6">
-    <div class="basis-1/2">4th Row</div>
-    <div class="basis-1/2">4th Row</div>
+      <div class="basis-1/2 max-w-[50%]">4th Row</div>
+      <div class="basis-1/2 max-w-[50%]">4th Row</div>
     </div>
     <div class="flex flex-row px-4 py-6">
-    <div class="basis-3/4">5th Row</div>
-    <div class="basis-1/4">5th Row</div>
+      <div class="basis-3/4 max-w-[75%]">5th Row</div>
+      <div class="basis-1/4 max-w-[25%]">5th Row</div>
     </div>
     <div class="flex flex-row px-4 py-6">
-    <div class="basis-1/2">6th Row</div>
-    <div class="basis-1/2">6th Row</div>
+      <div class="basis-1/2 max-w-[50%]">6th Row</div>
+      <div class="basis-1/2 max-w-[50%]">6th Row</div>
     </div>
     <div class="flex flex-row px-4 py-6">
-    <div class="basis-3/4">7th Row</div>
-    <div class="basis-1/4">7th Row</div>
+      <div class="basis-3/4 max-w-[75%]">7th Row</div>
+      <div class="basis-1/4 max-w-[25%]">7th Row</div>
     </div>
   </div>
 </template>
